aefb58bf4ee72a83c5be358043be42871fe00794
Sam Martinez <56923470+Sam-690@users.noreply.github.com>
2021-09-21 13:57:46 -0500

    NEW COMMIT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import {Route, Switch } from 'react-router-dom'
+
+import Heading from './components/Heading';
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
+import { GlobalProvider  } from './context/GlobalContext';
+
 import './App.css';
+
+
+console.log(GlobalProvider )
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen text-white text-center p-10">
+      <div className="container mx-auto h-full">
+        <GlobalProvider >
+          <Heading />
+          <Switch>
+            <Route path='/' component={TaskList} exact />   
+            <Route path='/add' component={TaskForm} exact />
+            <Route path='/edit/:id' component={TaskForm} exact />        
+        </Switch>
+        </GlobalProvider >
+      </div> 
     </div>
   );
 }
